3bb404aabfb8157d83a42eeaa3ecf9bab2ef5d1e
anyong <wrinkle905310338@163.com>
2019-02-27 14:40:02 +0800

    day1.1

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
        <HomeHeader></HomeHeader>
+        <Swiper></Swiper>
 
     </div>
 </template>
 
 <script>
     import HomeHeader from './components/HomeHeader'
+    import Swiper from './components/Swiper'
     export default {
         name: 'Home',
         components: {
             HomeHeader,
+            Swiper
         }
     }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="wraper">
+        <swiper :options="swiperOption" >
+                <!-- slides -->
+                <swiper-slide v-for="item of swiperList" :key="item.id">
+                    <img class="swiper-img" :src="item.imgUrl" />
+                </swiper-slide>
+                <!-- Optional controls -->
+                <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Homedswiper',
+        data() {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [{
+                    id: '0001',
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/59e329fb916e2a30bb64f26f0279b307.jpg_750x200_5c24cc64.jpg'
+                }, {
+                    id: '0002',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/a8/cb43c4ac6c215d02.jpg_750x200_83dee994.jpg'
+                }]
+            }
+        }
+    }
+</script>
+<style lang="stylus" scoped>
+    .wraper>>>.swiper-pagination-bullet-active {
+        background: #fff
+    }
+    
+    .wraper {
+        overflow: hidden;
+        width: 100%;
+        background: #eee;
+    }
+    
+    .wraper .swiper-img {
+        width: 100%;
+    }
+</style>
